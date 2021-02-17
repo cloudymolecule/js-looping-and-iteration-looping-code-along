c0605a0ac922f362c647eba79ba1b6d25bb40b9c
Cloudymolecule <baires82@gmail.com>
2021-02-17 11:10:24 -0500

    commit tree 0bff67326580f1e43b59c0d2459fb744dab99a98

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, message){
+    let arr = []
+    for(let i = 0; i < array.length; i++){
+        arr.push(`Thank you, ${array[i]}, for the wonderful ${message} gift!`);
+    }
+    return arr
+}
+
+function countDown(number){
+    let i = number
+    while (i >= 0){
+        console.log(i)
+        i = i - 1
+    }
+}
